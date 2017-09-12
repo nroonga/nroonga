870fd2e52eda762c7f7173e15a054d628d9678cb
abetomo <abe@enzou.tokyo>
2017-09-12 16:37:18 +0900

    Fix 'example/test.js' with reference to README example code

--- examples/test.js
+++ examples/test.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const nroonga = require('../lib/nroonga')
-
 const db = new nroonga.Database()
 
+// Synchronous
+console.log('// Synchronous')
+console.log(db.commandSync('status'))
+
+// Asynchronous
 db.command('status', (_, data) => {
+  console.log('// Asynchronous')
   console.log(data)
 })
